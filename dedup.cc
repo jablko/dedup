11911c29724e8708130891570a4dd0b8c46562dd
Jack Bates <jack@nottheoilrig.com>
2012-05-11 05:56:48 -0700

    First crack at check if URL already exist in cache

--- dedup.cc
+++ dedup.cc
@@ -1,6 +1,55 @@
 #include <stdio.h>
 #include <ts/ts.h>
 
+typedef struct {
+
+  TSHttpTxn txnp;
+  TSMBuffer bufp;
+  TSMLoc hdr_loc;
+  TSMLoc location_loc;
+
+} Info;
+
+static int
+handler(TSCont contp, TSEvent event, void *edata)
+{
+  Info *info = (Info*) TSContDataGet(contp);
+
+  switch (event) {
+  case TS_EVENT_CACHE_OPEN_READ:
+
+    TSHandleMLocRelease(info->bufp, info->hdr_loc, info->location_loc);
+    TSHandleMLocRelease(info->bufp, TS_NULL_MLOC, info->hdr_loc);
+
+    TSHttpTxnReenable(info->txnp, TS_EVENT_HTTP_CONTINUE);
+
+    TSfree(info);
+    TSContDestroy(contp);
+
+    break;
+
+  case TS_EVENT_CACHE_OPEN_READ_FAILED:
+
+    TSMimeHdrFieldValuesClear(info->bufp, info->hdr_loc, info->location_loc);
+    TSMimeHdrFieldValueStringInsert(info->bufp, info->hdr_loc, info->location_loc, -1, "http://example.com/", 19);
+
+    TSHandleMLocRelease(info->bufp, info->hdr_loc, info->location_loc);
+    TSHandleMLocRelease(info->bufp, TS_NULL_MLOC, info->hdr_loc);
+
+    TSHttpTxnReenable(info->txnp, TS_EVENT_HTTP_CONTINUE);
+
+    TSfree(info);
+    TSContDestroy(contp);
+
+    break;
+
+  default:
+    TSAssert(!"Unexpected event");
+  }
+
+  return 0;
+}
+
 static int
 dedup_plugin(TSCont contp, TSEvent event, void *edata)
 {
@@ -12,28 +61,75 @@ dedup_plugin(TSCont contp, TSEvent event, void *edata)
     TSMBuffer bufp;
     TSMLoc hdr_loc;
     TSMLoc location_loc;
+
+    const char *value;
+    int length;
+
+    TSMLoc url_loc;
+    TSCacheKey key;
+
     TSMLoc link_loc;
     TSMLoc next_loc;
 
     int idx;
     int count;
 
-    const char *value;
-    int length;
-
     if (TSHttpTxnClientRespGet(txnp, &bufp, &hdr_loc) != TS_SUCCESS) {
       TSError("Couldn't retrieve client request header\n");
 
-      goto done;
+      TSHttpTxnReenable(txnp, TS_EVENT_HTTP_CONTINUE);
+
+      break;
     }
 
+    /* Assumption: Want to minimize cache read, so check first that:
+     *
+     *   1. response has "Location: ..." header
+     *   2. response has RFC 6249 "Link: <...>; rel=duplicate" header
+     *
+     * Then scan if URL already exist in cache */
+
+    /* If response has "Location: ..." header */
     location_loc = TSMimeHdrFieldFind(bufp, hdr_loc, TS_MIME_FIELD_LOCATION, TS_MIME_LEN_LOCATION);
     if (!location_loc) {
       TSHandleMLocRelease(bufp, TS_NULL_MLOC, hdr_loc);
 
-      goto done;
+      TSHttpTxnReenable(txnp, TS_EVENT_HTTP_CONTINUE);
+
+      break;
     }
 
+    value = TSMimeHdrFieldValueStringGet(bufp, hdr_loc, location_loc, 0, &length);
+
+    /* If can't parse or lookup "Location: ..." URL, should still check if
+     * response has RFC 6249 "Link: <...>; rel=duplicate" header? No: Can't
+     * parse or lookup URL in "Location: ..." header is error */
+    TSUrlCreate(bufp, &url_loc);
+    if (TSUrlParse(bufp, url_loc, &value, value + length) != TS_PARSE_DONE) {
+
+      TSHandleMLocRelease(bufp, TS_NULL_MLOC, url_loc);
+      TSHandleMLocRelease(bufp, hdr_loc, location_loc);
+      TSHandleMLocRelease(bufp, TS_NULL_MLOC, hdr_loc);
+
+      TSHttpTxnReenable(txnp, TS_EVENT_HTTP_CONTINUE);
+
+      break;
+    }
+
+    key = TSCacheKeyCreate();
+    if (TSCacheKeyDigestFromUrlSet(key, url_loc) != TS_SUCCESS) {
+      TSCacheKeyDestroy(key);
+
+      TSHandleMLocRelease(bufp, TS_NULL_MLOC, url_loc);
+      TSHandleMLocRelease(bufp, hdr_loc, location_loc);
+      TSHandleMLocRelease(bufp, TS_NULL_MLOC, hdr_loc);
+
+      TSHttpTxnReenable(txnp, TS_EVENT_HTTP_CONTINUE);
+
+      break;
+    }
+
+    /* ... and RFC 6249 "Link: <...>; rel=duplicate" header */
     link_loc = TSMimeHdrFieldFind(bufp, hdr_loc, "Link", 4);
     while (link_loc) {
 
@@ -41,8 +137,24 @@ dedup_plugin(TSCont contp, TSEvent event, void *edata)
       for (idx = 0; idx < count; idx++) {
         value = TSMimeHdrFieldValueStringGet(bufp, hdr_loc, link_loc, idx, &length);
 
-        TSMimeHdrFieldValuesClear(bufp, hdr_loc, location_loc);
-        TSMimeHdrFieldValueStringInsert(bufp, hdr_loc, location_loc, -1, value, length);
+        contp = TSContCreate(handler, NULL);
+
+        Info *info = (Info*) TSmalloc(sizeof(Info));
+        info->txnp = txnp;
+        info->bufp = bufp;
+        info->hdr_loc = hdr_loc;
+        info->location_loc = location_loc;
+
+        TSContDataSet(contp, info);
+
+        TSCacheRead(contp, key);
+
+        TSCacheKeyDestroy(key);
+
+        TSHandleMLocRelease(bufp, hdr_loc, link_loc);
+        TSHandleMLocRelease(bufp, TS_NULL_MLOC, url_loc);
+
+        return 0;
       }
 
       next_loc = TSMimeHdrFieldNextDup(bufp, hdr_loc, link_loc);
@@ -52,10 +164,12 @@ dedup_plugin(TSCont contp, TSEvent event, void *edata)
       link_loc = next_loc;
     }
 
+    TSCacheKeyDestroy(key);
+
+    TSHandleMLocRelease(bufp, TS_NULL_MLOC, url_loc);
     TSHandleMLocRelease(bufp, hdr_loc, location_loc);
     TSHandleMLocRelease(bufp, TS_NULL_MLOC, hdr_loc);
 
-done:
     TSHttpTxnReenable(txnp, TS_EVENT_HTTP_CONTINUE);
 
     break;
